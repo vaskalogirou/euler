feb62f5358253e24964f8f070b3da2b981dd809b
kalogirou <vaskalogirou@gmail.com>
2019-06-16 23:48:13 +0300

    solved eleventh problem and added a helper method on IOUtils

--- src/kalogirou/euler/utilities/IOUtils.java
+++ src/kalogirou/euler/utilities/IOUtils.java
@@ -9,4 +9,18 @@ public class IOUtils {
 		String content = new String(Files.readAllBytes(Paths.get(path)));
 		return content;
 	}
+
+	public static int[][] readGrid(String grid) {
+		String[] lines = grid.split("\r\n");
+		int[][] result = new int[lines.length][];
+		for (int index = 0; index < lines.length; index++) {
+			String[] tokens = lines[index].trim().split(" ");
+			int[] numbers = new int[tokens.length];
+			for (int j = 0; j < tokens.length; j++) {
+				numbers[j] = Integer.valueOf(tokens[j]);
+			}
+			result[index] = numbers;
+		}
+		return result;
+	}
 }
